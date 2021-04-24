a9454b8871e5f81d4d16f2b9acb233fbaf89c8a1
Chuan Hong <chuanhong@Chuans-MBP.home>
2021-04-24 19:19:37 -0400

    add collision/crimeIndex layers

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { MapContainer, TileLayer, Polyline, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Marker, Popup, useMap, LayersControl, LayerGroup, Tooltip, CircleMarker,  } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import NavPanel from './NavPanel';
+import { COLORS, HEATS } from './constants';
+import collisions from './data/collisions-small.json';
+import crimeIndex from './data/crimes_index-small.json';
+require('react-leaflet-markercluster/dist/styles.min.css');
 
 const DefaultIcon = L.icon({
     iconUrl: icon,
@@ -21,7 +26,10 @@ L.Marker.prototype.options.icon = DefaultIcon;
 const center = [47.608013, -122.335167];
 const zoom = 13;
 
-const colors = ["#8AC926", "#1982C4", "#6A4C93", "#FF595E", "#FFCA3A"];
+const crimeFiltered = crimeIndex.filter( data => data.Index > 99).map( data  => ({
+  Color: HEATS[Math.min(HEATS.length, Math.floor(data.Index / 100))-1], 
+  LatLng: data.LatLng, Index: Math.round(data.Index)
+}));
 
 function SetViewOnChange({ nodes }) {
   const map = useMap();
@@ -47,14 +55,46 @@ function App() {
       <div id="map">
         <MapContainer center={center} zoom={zoom}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors" />
-          {segments.map((segment, index) =>
-            <Polyline weight={6} opacity={0.75} positions={segment} color={colors[index]} />
-          )}
-          {nodes.map(node =>
-            <Marker position={node.latlng} >
-              <Popup>{node.loc.join(' & ')}</Popup>
-            </Marker>
-          )}
+          <LayersControl position="topleft">
+            <LayersControl.Overlay checked name="Navigation">
+            <LayerGroup>
+              {segments.map((segment, index) =>
+                <Polyline weight={6} opacity={0.75} positions={segment} color={COLORS[index]} />
+              )}
+              {nodes.map(node =>
+                <Marker position={node.latlng} >
+                  <Popup>{node.loc.join(' & ')}</Popup>
+                </Marker>
+              )}
+            </LayerGroup>
+            </LayersControl.Overlay>
+            <LayersControl.Overlay name="Collisions">
+              <MarkerClusterGroup>
+                {collisions.map( data => {
+                  return (
+                  <CircleMarker center={data.LatLng}>
+                    <Tooltip direction="right" offset={[0, 0]} opacity={1}>
+                      <p align="left">Date: {data.INCDATE}</p>
+                      <p>{data.SDOT_COLDESC}</p>
+                    </Tooltip>
+                  </CircleMarker>
+                  )
+                })}
+              </MarkerClusterGroup>;
+            </LayersControl.Overlay>
+            <LayersControl.Overlay checked name="Crime Index">
+              <LayerGroup>
+                {crimeFiltered.map( data => {
+                  return (
+                    <CircleMarker center={data.LatLng} radius={10} 
+                      pathOptions={{ color: data.Color, fillOpacity: 0.8, stroke: false}}>
+                      <Tooltip direction="right" offset={[0, 0]} opacity={1}>{data.Index}</Tooltip>
+                    </CircleMarker>
+                    );
+                })}
+              </LayerGroup>
+            </LayersControl.Overlay>
+          </LayersControl>
           <SetViewOnChange nodes={nodes} />
         </MapContainer>
       </div>
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const COLORS = ["#8AC926", "#1982C4", "#6A4C93", "#FF595E", "#FFCA3A"];
+
+export const HEATS = ["#ffba08", "#faa307", "#f48c06", "#e85d04", "#dc2f02", "#d00000", "#9d0208", "#6a040f"];
+
